14e38fcb734a181a9dff2e7e307a81654136f835
SOSA1095 <andressosasilva@hotmail.com>
2018-12-04 21:37:35 -0600

    pushing DB

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ class ProductionConfig(Config):
     # os.environ.get("DATABASE_URL")
     # postgres://sbmvjnstodirdq:da2890133f95e99bfcf7cb5a3631e9b164ce2e8cd570e235aa718ba47a83230b@ec2-174-129-32-37.compute-1.amazonaws.com:5432/demqd7v1u6naso
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgres://sbmvjnstodirdq:da2890133f95e99bfcf7cb5a3631e9b164ce2e8cd570e235aa718ba47a83230b@ec2-174-129-32-37.compute-1.amazonaws.com:5432/demqd7v1u6naso"
-    DATABASE_URL = "postgres://sbmvjnstodirdq:da2890133f95e99bfcf7cb5a3631e9b164ce2e8cd570e235aa718ba47a83230b@ec2-174-129-32-37.compute-1.amazonaws.com:5432/demqd7v1u6naso"
+    SQLALCHEMY_DATABASE_URI = "postgres://iywlwaiirtbmey:4dc856b8e031323d34e1e182df5a94e1771fbdce7712b0b49252749952381a42@ec2-107-21-125-209.compute-1.amazonaws.com:5432/dbk2tngq2jv76q"
+    DATABASE_URL = "postgres://iywlwaiirtbmey:4dc856b8e031323d34e1e182df5a94e1771fbdce7712b0b49252749952381a42@ec2-107-21-125-209.compute-1.amazonaws.com:5432/dbk2tngq2jv76q"
 
 
 class StagingConfig(Config):
