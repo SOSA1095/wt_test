9398cb9b1900a5a61fd5c9a93f333a26a4cfba8f
SOSA1095 <andressosasilva@hotmail.com>
2018-12-04 22:39:05 -0600

    top kek

--- app.py
+++ app.py
@@ -41,10 +41,7 @@ from flask_cors import CORS, cross_origin
 def create_app():
     app = Flask(__name__)
     db.init_app(app)
-    with app.app_context():
-        # Extensions like Flask-SQLAlchemy now know what the "current" app
-        # is while within this block. Therefore, you can now run........
-        db.create_all()
+    app = app.app_context().push()
     return app
 
 # app = create_app()
